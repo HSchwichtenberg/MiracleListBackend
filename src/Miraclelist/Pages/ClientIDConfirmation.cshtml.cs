0ffbe1eb99fc48588f91d31bb57914aa0bc8b169
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-02-16 16:40:11 +0100

    ViewComponent verwendet

--- src/Miraclelist/Pages/ClientIDConfirmation.cshtml.cs
+++ src/Miraclelist/Pages/ClientIDConfirmation.cshtml.cs
@@ -7,17 +7,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Miraclelist_WebAPI.Pages
 {
+ public class Info
+ {
+  public string Name { get; set; }
+  public string EMail { get; set; }
+ }
  public class ClientIDConfirmationModel : PageModel
  {
 
+  
   [TempData]
   public string EMail { get; set; }
   [TempData]
   public string Name { get; set; }
-
+  [BindProperty]
+  public Info Info { get; set; }
   public void OnGet()
   {
-
+   //(string Name, string EMail) info = (this.Name, this.EMail);
+   this.Info = new Info { Name = this.Name, EMail = this.EMail };
   }
  }
 }
