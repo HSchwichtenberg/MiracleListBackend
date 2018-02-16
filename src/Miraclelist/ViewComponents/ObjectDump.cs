b1a62044d90d9b1802559fe9a836a445ad7601e4
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-02-16 16:40:11 +0100

    ViewComponent verwendet

---
+++ src/Miraclelist/ViewComponents/ObjectDump.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+
+
+namespace ITVisions.Components
+{
+ [ViewComponent(Name = "ObjectDump")]
+ public class ObjectDumpComponentController : ViewComponent
+ {
+
+  public ObjectDumpComponentController()
+  {
+
+  }
+
+  public IViewComponentResult Invoke(object obj, bool? details = false)
+  {
+
+   var modell = obj.ToNameValueDictionary().Where(x => !x.Key.Contains("BackingField")).ToList();
+    return View(modell);
+  }
+ }
+}
