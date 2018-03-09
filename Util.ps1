f7d91a2fe962f0faf0eab4a71976e0184db502ad
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-03-09 10:03:16 +0100

    Fix Falscher Aufruf bei GetUserStatistics()

--- Util.ps1
+++ Util.ps1
@@ -1,7 +1,7 @@
 cls
 $ENVName = "ConnectionStrings:MiracleListDB"
-$CS = "Data Source=D120;Initial Catalog=MiracleList_Test;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-$CS = "InMemoryDB"
+$CS = "Data Source=.;Initial Catalog=MiracleList_Test;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+#$CS = "InMemoryDB"
 "Set ConnectionStrings:MiracleListDB = $CS ..."
 [Environment]::SetEnvironmentVariable("ConnectionStrings:MiracleListDB", $CS, "Machine")
 $env:ConnectionStrings:MiracleListDB = $CS
