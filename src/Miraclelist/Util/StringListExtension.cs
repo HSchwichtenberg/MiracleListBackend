817764356a399860de1f76d5a7f9cb6652ef4dd1
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-03-10 08:29:32 +0100

    c

--- src/Miraclelist/Util/StringListExtension.cs
+++ src/Miraclelist/Util/StringListExtension.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
-namespace ITVisions.Extensions
+namespace ITVisions.AspNetCore
 {
  public static class StringListExtension
  {
