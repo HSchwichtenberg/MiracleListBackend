117f76d386deab320d2925f10f36ffc2f327e37d
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-03-21 12:41:02 +0100

    deploymentskriot verbessert für Demo auf Linux

--- publishSCA.ps1
+++ publishSCA.ps1
@@ -4,4 +4,7 @@ $ziel = "t:\mlb"
 # Self-contained deployment (SCD) / Self-contained application (SCA)
 dotnet publish -c release --runtime ubuntu.14.04-x64 --self-contained --framework netcoreapp2.0 -o $ziel
 
-robocopy $ziel \\172.20.31.156\Documents\MLB
+robocopy $ziel \\192.168.1.170\Documents\MLB /e
+
+
+robocopy $PSScriptRoot\ \\192.168.1.170\Documents\MLB_Source /e
