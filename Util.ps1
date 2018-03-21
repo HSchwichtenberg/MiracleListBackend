a884c3ed1c0384b361b6e76b7fe9dc495320e6e8
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2018-03-21 13:32:23 +0100

    Ausgaben verbessert

--- Util.ps1
+++
@@ -1,9 +0,0 @@
-cls
-$ENVName = "ConnectionStrings:MiracleListDB"
-$CS = "Data Source=.;Initial Catalog=MiracleList_Test;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
-#$CS = "InMemoryDB"
-"Set ConnectionStrings:MiracleListDB = $CS ..."
-[Environment]::SetEnvironmentVariable("ConnectionStrings:MiracleListDB", $CS, "Machine")
-$env:ConnectionStrings:MiracleListDB = $CS
-$PSScriptRoot
-dotnet test $PSScriptRoot\Test\UnitTests\UnitTests.csproj --filter Category!=Integration
