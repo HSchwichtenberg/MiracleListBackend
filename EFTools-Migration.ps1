4cb0d5fc6a4ed47f036e63be2c11fa7fa7e682d7
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-08-21 11:57:13 +0200

    Update von .NET Core 2.0 auf .NET Core 2.1.2

--- EFTools-Migration.ps1
+++ EFTools-Migration.ps1
@@ -1,7 +1,9 @@
 ﻿
 Cd $PSScriptRoot\src\EFCTools\bin\debug\netcoreapp2.0
 
-$env:ConnectionStrings:MiracleListDB = "Data Source=localhost;Initial Catalog=MiracleList_Test;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+$env:ConnectionStrings:MiracleListDB = "Data Source=localhost;Initial Catalog=MiracleList_TEMP2;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
 
 dotnet .\EFCTools.dll migrate 
 
+
+#dotnet .\EFCTools.dll recreate 
