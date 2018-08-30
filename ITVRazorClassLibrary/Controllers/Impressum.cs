dd8ff3dfda5b6820f8df5fac9732a80784282742
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-08-30 15:45:48 +0200

    mehr zu RCL

---
+++ ITVRazorClassLibrary/Controllers/Impressum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ITVRazorClassLibrary.Controllers
+{
+ public class Impressum : Controller
+ {
+  // GET: /<controller>/
+  public IActionResult Index()
+  {
+
+   ViewBag.Adresse = @"Max Mustermann (Inhaber)<BR>
+ Musterfirma<BR>
+ Musterstraße 40b<br>
+ D-12345 Musterstadt (Deutschland)";
+   return View();
+  }
+ }
+}
