a8fa8f2b48e0d01b45e74cc087e998e9f369190f
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-08-30 15:45:48 +0200

    mehr zu RCL

--- ITVRazorClassLibrary/Pages/Imprint.cshtml.cs
+++ ITVRazorClassLibrary/Pages/Imprint.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ITVRazorClassLibrary.MyFeature.Pages
+namespace ITVRazorClassLibrary.Pages
 {
- public class Page1Model : PageModel
+ public class ImprintModel : PageModel
  {
   public void OnGet()
   {
