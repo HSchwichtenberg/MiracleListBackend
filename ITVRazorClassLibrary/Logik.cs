23d470a8a87edb1d0c66118e959e55c86391f266
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-08-30 15:45:48 +0200

    mehr zu RCL

---
+++ ITVRazorClassLibrary/Logik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITVRazorClassLibrary
+{
+ public class Logik
+ {
+
+  public static string GetVersion()
+  {
+   return "2018-08-30";
+
+  }
+ }
+}
---
+++ src/Miraclelist/Controllers/Impressum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ITVRazorClassLibrary.Controllers
+{
+ public class Impressum : Controller
+ {
+  // GET: /<controller>/
+  public IActionResult Index()
+  {
+
+   ViewBag.Adresse = @"Dr. Holger Schwichtenberg (Owner)<BR>
+ www.IT-Visions.de<BR>
+ Fahrenberg 40b<br>
+ D-45257 Essen / Ruhrgebiet (Germany)";
+   return View();
+  }
+ }
+}
