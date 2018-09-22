700ae0df8529b9b3aaf7a99ce242c68ff14c0fd2
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-09-22 17:38:19 +0200

    Demo zu Shadow State  verbessert

---
+++ src/DAL/SQL/AdditionalColumns.sql
@@ -0,0 +1,16 @@
+﻿USE MiracleList_Test;
+GO
+ALTER table [user] add  City nvarchar(MAX);
+alter table [user] add Important bit null;
+
+UPDATE [user] SET city = 'Mainz' WHERE UserID % 5 = 0
+UPDATE [user] SET city = 'Essen' WHERE UserID % 3 = 0
+UPDATE [user] SET Important = (select convert(bit, round(1*rand(),0))) WHERE UserID % 2 = 0
+
+
+USE MiracleList_Test;
+GO
+
+ALTER table [user] drop COLUMN  City ;
+
+ALTER table [user] drop COLUMN  Important ;
