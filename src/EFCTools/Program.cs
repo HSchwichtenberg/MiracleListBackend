1e3ef1f75e25d18d81b3cdf888c4250ce8cf0a97
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-09-23 21:07:49 +0200

    Aufräumen

--- src/EFCTools/Program.cs
+++ src/EFCTools/Program.cs
@@ -1,17 +1,14 @@
 ﻿using BO;
 using DAL;
 using ITVisions;
+using ITVisions.EFC;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using ITVisions.EFCore;
-using ITVisions.EFC;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace EFTools
 {
