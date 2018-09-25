7f7d240d372423394d480e68ee08f74916b72da3
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-09-25 12:37:40 +0200

    mehr unit tests

--- Test/UnitTests/LogManagerTest.cs
+++ Test/UnitTests/LogManagerTest.cs
@@ -22,12 +22,14 @@ namespace UnitTests
    public void Log()
    {
    var id = new LogManager().Log(Event.LoginOK, Severity.Information, "Test", "test", "none");
-   }
+   Assert.True(id > 0);
+  }
   [Fact]
   public void LogWithUser()
   {
    var um = new UserManager("logtestuser", true);
    var id = new LogManager().Log(Event.LoginOK, Severity.Information, "Test", "test", "none", um.CurrentUser.UserID);
+   Assert.True(id > 0);
   }
 
  
