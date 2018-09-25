4b7545ad2fae3f77c49d3bb68614047ad5df243c
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-09-25 12:37:40 +0200

    mehr unit tests

---
+++ Test/UnitTests/CategoryManagerTest.cs
@@ -0,0 +1,35 @@
+﻿using BL;
+using BO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests
+{
+ public class CategoryManagerTest
+ {
+  private const string testcat = "testcat";
+
+  public CategoryManagerTest()
+   {
+    Util.Init();
+   }
+
+   [Fact]
+
+   public void Log()
+   {
+   var um = new UserManager("cattestuser " + System.DateTime.Now.Ticks, true);
+   
+   var c = new Category();
+   c.Name = testcat;
+   c.UserID = um.CurrentUser.UserID;
+   var cm = new CategoryManager(um.CurrentUser.UserID);
+   var c2 = cm.New(c);
+   Assert.Equal(testcat, c2.Name);
+   }
+
+ }
+}
+
