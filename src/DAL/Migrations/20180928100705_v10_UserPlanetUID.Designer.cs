dfd6c975d101ccab126d30a9287d83f49a0989ed
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-09-28 13:06:23 +0200

    mit PlanetUID

--- src/DAL/Migrations/20180928100705_v10_UserPlanetUID.Designer.cs
+++ src/DAL/Migrations/20180928100705_v10_UserPlanetUID.Designer.cs
@@ -4,14 +4,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20180928100705_v10_UserPlanetUID")]
+    partial class v10_UserPlanetUID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
