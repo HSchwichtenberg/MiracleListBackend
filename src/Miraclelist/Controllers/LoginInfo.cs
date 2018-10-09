7a77a49b34f562023ec97145e0273d3efbba8185
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-10-10 01:01:34 +0200

    0.11: Ausgabe AppVersion

---
+++ src/Miraclelist/Controllers/LoginInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Miraclelist.Controllers
+{
+ /// <summary>
+ /// DTO
+ /// </summary>
+ public class LoginInfo
+ {
+  public string ClientID;
+  public string Username;
+  public string Password;
+  public string Token;
+  public string Message;
+ }
+}
