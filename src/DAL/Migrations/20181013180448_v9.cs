5f3bcea162aa2f1416f871ba8b25f81faaabb9ca
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2018-10-13 20:07:43 +0200

    FIX: 0.11.2: DueInDays muss nullable sein, da Due nullable ist. Due )= null bisher durch Unit Tests nicht geprüft :-(

---
+++ src/DAL/Migrations/20181013180448_v9.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class v9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DueInDays",
+                table: "Task",
+                nullable: true,
+                computedColumnSql: "DATEDIFF(day, GETDATE(), [Due])",
+                oldClrType: typeof(int),
+                oldComputedColumnSql: "DATEDIFF(day, GETDATE(), [Due])");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DueInDays",
+                table: "Task",
+                nullable: false,
+                computedColumnSql: "DATEDIFF(day, GETDATE(), [Due])",
+                oldClrType: typeof(int),
+                oldNullable: true,
+                oldComputedColumnSql: "DATEDIFF(day, GETDATE(), [Due])");
+        }
+    }
+}
