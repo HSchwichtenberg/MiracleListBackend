34587923faf0bec0f046066abaa6883106927afe
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-02-19 22:20:22 +0100

    mehr JS

--- src/MiracleList_RC/MiracleList_RC.Server/wwwroot/Util.js
+++ src/MiracleList_RC/MiracleList_RC.Server/wwwroot/Util.js
@@ -2,7 +2,7 @@
  console.log(s);
 };
 
-//window.confirm = (text1, text2) => {
-// console.log("confirm", text1, text2);
-//     return confirm(text1 + "\n" + text2);
-//   };
+window.GetConfirmation = (text1, text2) => {
+ console.log("GetConfirmation", text1, text2);
+     return confirm(text1 + "\n" + text2);
+   };
