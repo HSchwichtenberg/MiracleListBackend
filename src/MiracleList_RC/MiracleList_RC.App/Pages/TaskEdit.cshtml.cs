79eeb4fbe2fd03e33d054104d41ac130a25a4a94
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-02-19 18:13:29 +0100

    +taskedit cmponents

---
+++ src/MiracleList_RC/MiracleList_RC.App/Pages/TaskEdit.cshtml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace MiracleList_RC.App.Pages
+{
+ public class TaskEditModel : ComponentBase
+ {
+
+ }
+}
---
+++ src/MiracleList_RC/MiracleList_RC.App/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiracleList_RC.App
+{
+ class console
+ {
+
+  //public async void log(string s)
+  //{
+  // await JsRuntime.InvokeAsync<string>("log", "ML: " + s);
+
+  // this.StateHasChanged();
+  //}
+ }
+}
