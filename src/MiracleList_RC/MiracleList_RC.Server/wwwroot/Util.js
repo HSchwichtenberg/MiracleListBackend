e3530882e728a78a2259936f23b17df187c6ff3e
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-02-19 21:33:55 +0100

    Remove Buttons

---
+++ src/MiracleList_RC/MiracleList_RC.Server/wwwroot/Util.js
@@ -0,0 +1,8 @@
+﻿window.log = (s) => {
+ console.log(s);
+};
+
+//window.confirm = (text1, text2) => {
+// console.log("confirm", text1, text2);
+//     return confirm(text1 + "\n" + text2);
+//   };
