8c8c1cab34745ed56841cee9223b5be08a38a73a
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-02-24 23:53:43 +0100

    Refactoring Unit Tests

--- Test/UnitTests/UserManagerTest.cs
+++ Test/UnitTests/UserManagerTest.cs
@@ -20,7 +20,7 @@ namespace UnitTests
   [Trait("Category", "Integration")]
   public void GetLatestUsersTest()
   {
-   Skip.If(DAL.Context.ConnectionString == "", "Only runs as integration test as the InMem-DB does not support SQL!");
+   Skip.If(Util.IsInMemory, "Only runs as integration test as the InMem-DB does not support SQL!");
    var um = new UserManager("test", true);
    var stat = UserManager.GetLatestUserSet();
    Assert.True(stat.Count > 0);
