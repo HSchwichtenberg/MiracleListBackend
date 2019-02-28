1e6821d8b0dc095bfc2486b531aa563743dc8c75
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-02-28 15:25:52 +0100

    fix

--- DevOps_Release/Postman-Test.ps1
+++ DevOps_Release/Postman-Test.ps1
@@ -1,4 +1,6 @@
 # Startet Postman Test gegen Entwicklungsrechner
 cls
 cd $PSScriptRoot
-newman run MiracleList-APITests.postman_collection.json -e MiracleList-APITests.postman_environment.dev.json --disable-unicode --reporters cli,junit --reporter-junit-export TestResults/newman-run-report.xml
+newman run MiracleList-APITests.postman_collection.json `
+-e MiracleList-APITests.postman_environment.dev.json --disable-unicode `
+--reporters cli,junit --reporter-junit-export TestResults/newman-run-report.xml
