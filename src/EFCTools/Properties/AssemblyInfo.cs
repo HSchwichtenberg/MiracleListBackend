a2a33413b3a0bac587b96f4b3e87331834afdc0c
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-04-03 13:30:11 +0200

    EFTools 1.1 mit verbesserter Ausgabe

--- src/EFCTools/Properties/AssemblyInfo.cs
+++ src/EFCTools/Properties/AssemblyInfo.cs
@@ -5,9 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EFTools")]
+
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
