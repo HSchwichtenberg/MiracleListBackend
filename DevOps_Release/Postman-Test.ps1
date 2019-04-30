7514eb39adbce7058f0568da536fd7d3e7be8ea3
Dr. Holger Schwichtenberg <git@IT-Visions.de>
2019-04-30 15:58:58 +0200

    Mehr jMeter Tests, Staging-Env für Postman

--- DevOps_Release/Postman-Test.ps1
+++ DevOps_Release/Postman-Test.ps1
@@ -2,5 +2,5 @@
 cls
 cd $PSScriptRoot
 newman run MiracleList-APITests.postman_collection.json `
--e MiracleList-APITests.postman_environment.dev.json --disable-unicode `
+-e MiracleList-APITests.postman_environment.staging.json --disable-unicode `
 --reporters cli,junit --reporter-junit-export TestResults/newman-run-report.xml
